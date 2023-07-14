46651f5f07fcdb00c6fde044e1a5ea4b995cb414
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-15 01:41:37 +0500

    Update Building.java

--- Building.java
+++ Building.java
@@ -5,17 +5,10 @@ public class Building implements Health {
     private int maxHealthPoint; // максимально количество здоровья
     private int currentHealthPoint; // текущее количество здоровья
 
-    public int getMaxHealthPoint() {
-        return maxHealthPoint;
-    }
-
-    public int getCurrentHealthPoint() {
-        return currentHealthPoint;
-    }
-
     public Building(int maxHealthPoint) {
         this.maxHealthPoint = maxHealthPoint;
         this.currentHealthPoint = maxHealthPoint;
+        System.out.println("Building");
     }
 
     public void setCurrentHealthPoint(int currentHealthPoint) {
@@ -23,6 +16,16 @@ public class Building implements Health {
     }
 
     // FIXME: 29.06.2023 Дописать нужное
+
+    @Override
+    public int getCurrentHealthLevel() {
+        return currentHealthPoint;
+    }
+
+    @Override
+    public int getMaxHealthLevel() {
+        return maxHealthPoint;
+    }
 
     @Override
     public void showCurrentHealthLevel() {
