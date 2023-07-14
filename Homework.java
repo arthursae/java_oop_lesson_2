d878ff0cd3ee1a29ad5cfe367ebebf34e6875595
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-15 01:39:00 +0500

    Update Homework.java

--- Homework.java
+++ Homework.java
@@ -1,27 +1,32 @@
 package SomeGame;
 
-import SomeGame.Building;
-
 public class Homework {
 
     public static void main(String[] args) {
         Render render = new Render();
 
-        Building building = new Building(100);
-        building.setCurrentHealthPoint(50);
-        render.showIndicator(building); // В консоли должно быть примерно так: Текущий уровень здоровья: 50, максимальный уровень здоровья: 100
-        //render.showMeter();
+        Building building1 = new Building(100);
+        building1.setCurrentHealthPoint(25);
+        //render.showIndicator(building1); // В консоли должно быть примерно так: Текущий уровень здоровья: 50, максимальный уровень здоровья: 100
+        render.showHealthBar(building1);
+
+        Building building2 = new Building(100);
+        building2.setCurrentHealthPoint(50);
+        //render.showIndicator(building2); // В консоли должно быть примерно так: Текущий уровень здоровья: 50, максимальный уровень здоровья: 100
+        render.showHealthBar(building2);
 
         Neutral neutral = new Neutral(100);
         neutral.setCurrentHealthPoint(75);
-        render.showIndicator(neutral);
-        //render.showMeter();
+        //render.showIndicator(neutral);
+        render.showHealthBar(neutral);
 
         Hero hero = new Hero(100, 100);
-        hero.setCurrentHealthPoint(90);
+        hero.setCurrentHealthPoint(100);
         hero.setCurrentManaPoint(55);
-        render.showIndicator(hero);
-        //render.showMeter();
+        //render.showIndicator(hero);
+        render.showHealthBar(hero);
+        render.showManaBar(hero);
+
 
     }
 }
