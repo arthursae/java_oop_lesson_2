7a3f775dc7b0c5684e7d6779ecab8550ad4e6125
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-14 21:44:20 +0500

    Create Neutral.java

---
+++ Neutral.java
@@ -0,0 +1,67 @@
+package SomeGame;
+
+public class Neutral implements Health {
+    public int getMaxHealthPoint() {
+        return maxHealthPoint;
+    }
+
+    public int getCurrentHealthPoint() {
+        return currentHealthPoint;
+    }
+
+    private int maxHealthPoint; // максимально количество здоровья
+    private int currentHealthPoint; // текущее количество здоровья
+
+    public Neutral(int maxHealthPoint) {
+        this.maxHealthPoint = maxHealthPoint;
+        this.currentHealthPoint = maxHealthPoint;
+    }
+
+    public void setCurrentHealthPoint(int currentHealthPoint) {
+        this.currentHealthPoint = currentHealthPoint;
+    }
+
+    // FIXME: 29.06.2023 Дописать нужное
+
+    @Override
+    public void showCurrentHealthLevel() {
+        System.out.println(currentHealthPoint);
+    }
+
+    @Override
+    public void showMaxHealthLevel() {
+        System.out.println(maxHealthPoint);
+    }
+
+    @Override
+    public void increaseHealth(int points) {
+        if (currentHealthPoint < maxHealthPoint) {
+            currentHealthPoint += points;
+
+            if (currentHealthPoint > maxHealthPoint) {
+                currentHealthPoint = maxHealthPoint;
+            }
+        } else {
+            currentHealthPoint = maxHealthPoint;
+        }
+    }
+
+    @Override
+    public void decreaseHealth(int points) {
+        if (currentHealthPoint > 0) {
+            currentHealthPoint -= points;
+            if (currentHealthPoint < 0) {
+                currentHealthPoint = 0;
+                die();
+            }
+        } else {
+            currentHealthPoint = 0;
+            die();
+        }
+    }
+
+    @Override
+    public void die() {
+        System.out.println("The neutral character has died");
+    }
+}
