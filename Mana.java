73d0fff53aacc2f60a77d8031ab7ad06def45f09
arthursae <118846805+arthursae@users.noreply.github.com>
2023-07-14 21:45:02 +0500

    Create Mana.java

---
+++ Mana.java
@@ -0,0 +1,13 @@
+package SomeGame;
+
+public interface Mana {
+    void showCurrentManaLevel();
+
+    void showMaxManaLevel();
+
+    void increaseManaLevel(int points);
+
+    void decreaseManaLevel(int points);
+
+    void helpless();
+}
